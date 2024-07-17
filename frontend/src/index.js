bb0633f02a5b5133a427092756f06e9b204ecb0c
Eng-Liban <LibanAbdulahi0008@gmail.com>
2024-07-17 20:00:26 +0300

    file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AuthContext from "./Context/Auth-Context";
 const container = document.querySelector("#root");
-export const endpoint = "http://localhost:4000";
+export const endpoint = "https://hubal-student-project.onrender.com";
 const root = createRoot(container);
 root.render(
   <AuthContext>
